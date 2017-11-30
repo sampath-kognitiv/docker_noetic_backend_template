706c8c5604686dfd9f97aa72e8802db3cc99e0c8
sampath-noetic <sampath.ekanayake@wearenoetic.com>
2017-11-30 08:12:53 +0530

    edited

--- mysql/99-grant_permission.sql
+++ mysql/99-grant_permission.sql
@@ -1,5 +1,5 @@
 GRANT ALL PRIVILEGES ON imperial.* TO 'root'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON imperial.* TO 'root'@'%' IDENTIFIED BY '';
 
-CREATE DATABASE craft_imperial;
-GRANT ALL PRIVILEGES ON craft_imperial.* TO 'root'@'%' IDENTIFIED BY '';
+-- CREATE DATABASE craft_imperial;
+-- GRANT ALL PRIVILEGES ON craft_imperial.* TO 'root'@'%' IDENTIFIED BY '';
